f884d34e764c03f0bb1e8d635fa1ba3bfea612a7
Eva van Geenen <eva.vangeenen@incentro.com>
2019-10-09 15:26:44 +0200

    saved both versions of persisting course and student relations

--- src/com/luv2code/hibernate/demo/CreateStudentWithCourse.java
+++ src/com/luv2code/hibernate/demo/CreateStudentWithCourse.java
@@ -30,8 +30,20 @@ public class CreateStudentWithCourse {
 			
 			session.beginTransaction();
 			
-			Course tempCourse = new Course("MyThirdCourse");
+	/*		Course tempCourse = new Course("MyFifthCourse");
+			session.save(tempCourse);
 			
+			Student student = new Student("123123", "Ak21313a", "mail");
+			Student student2 = new Student("Tu2131314ki", "Ol2113o", "meeuw");
+			
+			tempCourse.addStudent(student);
+			tempCourse.addStudent(student2);
+			
+			session.save(student);
+			session.save(student2);
+			*/
+			
+			Course tempCourse = new Course("MyThirdCourse");		
 			
 			Student student = new Student("Hama", "Okiodu", "beeuw");
 			Student student2 = new Student("Tama", "werwke", "leeuw");
@@ -39,9 +51,10 @@ public class CreateStudentWithCourse {
 			tempCourse.addStudent(student);
 			tempCourse.addStudent(student2);
 			
-			session.persist(tempCourse);
-			  		
+			session.persist(tempCourse);	
+			
 			session.getTransaction().commit();
+			
 			
 
 		} catch (Exception e) {
